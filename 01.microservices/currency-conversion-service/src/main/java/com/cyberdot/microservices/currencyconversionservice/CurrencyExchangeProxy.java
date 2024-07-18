bbc670961a97c14d2fefee65adbac88c78807ce0
safar <smahma100@gmail.com>
2024-07-18 12:09:53 -0700

    Reorganize project structure and move README.md to root

---
+++ 01.microservices/currency-conversion-service/src/main/java/com/cyberdot/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.cyberdot.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
+public interface CurrencyExchangeProxy {
+
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to);
+
+}
---
+++ 01.microservices/limits-service/src/main/java/com/cyberdot/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,24 @@
+package com.cyberdot.microservices.limitsservice.controller;
+
+import com.cyberdot.microservices.limitsservice.bean.Limits;
+import com.cyberdot.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+
+        return new Limits(configuration.getMinimum(),
+                configuration.getMaximum());
+        // return new Limits(1, 1000);
+
+    }
+}
