48bb177c1a4a3a217010fa1dcb6787aee470d2e6
safar <smahma100@gmail.com>
2024-07-18 13:41:13 -0700

    Add SpringBootProjects to 02.docker directory

---
+++ 02.docker/SpringBootProjects/api-gateway/src/main/java/com/cyberdot/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,39 @@
+package com.cyberdot.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.support.RouterFunctionMapping;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
