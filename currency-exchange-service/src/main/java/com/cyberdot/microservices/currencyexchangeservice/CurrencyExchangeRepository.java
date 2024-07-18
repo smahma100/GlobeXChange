96dbd0c856cb9d9a95032616ec916a7815e37592
safar <smahma100@gmail.com>
2024-07-18 12:32:01 -0700

    Clean up and organize project structure

--- currency-exchange-service/src/main/java/com/cyberdot/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cyberdot.microservices.currencyexchangeservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
-
-    CurrencyExchange findByFromAndTo(String from, String to);
-}
